8f0ff429663a85d8ad0831d16f05baaba5c7c542
wangh <wanghan@DESKTOP-3S9VQOF>
2016-07-19 14:40:52 +0800

    抓取所有页面的图片

--- crawler.py
+++ crawler.py
@@ -5,40 +5,64 @@ import urllib2
 import os
 from bs4 import BeautifulSoup
 
-url = ''
+name_count = 0
 
-#url = 'https://www.douban.com/photos/album/1617100987/'
+def crawl_from_url(url):
+	global name_count
 
-request = urllib2.Request(url)
+	request = urllib2.Request(url)
+	response = urllib2.urlopen(request)
+
+	html_doc = response.read()
+
+	soup = BeautifulSoup(html_doc, "lxml")
+
+	item = {}
+
+	pic_all = soup.findAll("div", {"class": 'photo_wrap'})
+	item['picUrl'] = []
+	item['picDesc'] = []
+
+	for pic in pic_all:
+		item['picUrl'].append(pic.img['src'])
+		item['picDesc'].append(pic.a['title'])
+
+	di = zip(item['picUrl'], item['picDesc'])
+
+	for i in range(len(di)):
+		each_pic = di[i]
+		
+		url = each_pic[0].replace("thumb", "photo")
+		#url = each_pic[0].replace("thumb", "large")
+		#desc = each_pic[1][0:8]
+		image = urllib.URLopener()  
+		image.retrieve(url, str(name_count)  + ".jpg")
+		name_count = name_count + 1
+
+
+first_url = 'http://www.douban.com/photos/album/155343624/'
+
+request = urllib2.Request(first_url)
 response = urllib2.urlopen(request)
 
 html_doc = response.read()
-
 soup = BeautifulSoup(html_doc, "lxml")
 
-item = {}
-
-pic_all = soup.findAll("div", {"class": 'photo_wrap'})
-item['picUrl'] = []
-item['picDesc'] = []
-
-for pic in pic_all:
-	item['picUrl'].append(pic.img['src'])
-	item['picDesc'].append(pic.a['title'])
+urls = soup.find("div", {"class": "paginator"}).find_all('a')
 
 folder_name = "pic"
-
 os.mkdir(folder_name)
 os.chdir(folder_name)
 
 
-di = zip(item['picUrl'], item['picDesc'])
+crawl_from_url(first_url)
 
-for i in range(len(di)):
-	each_pic = di[i]
-	
-	url = each_pic[0].replace("thumb", "photo")
-	#url = each_pic[0].replace("thumb", "large")
-	desc = each_pic[1][0:8]
-	image = urllib.URLopener()  
-	image.retrieve(url, str(i)  + ".jpg")
+for i in range(len(urls)):
+	url = urls[i]['href']
+	crawl_from_url(url)
+
+print "oo"
+
+
+
+
